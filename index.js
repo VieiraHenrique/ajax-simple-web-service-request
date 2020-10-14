b7c125e68ba6c897f17725627666efa5cbf34776
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2020-10-14 10:22:42 +0200

    add quote generation functionality

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const quote = document.getElementById('quote');
+const author = document.getElementById('author');
 const run = document.getElementById('run');
 
-run.addEventListener('click', ()=>{
-    console.log('clicked');
-})
+function getQuote(){
+    fetch('https://thatsthespir.it/api')
+    .then(res => res.json())
+    .then((data)=>{
+        printQuote(data)
+    })
+}
+
+function printQuote(data){
+    quote.innerHTML = data.quote;
+    author.innerHTML = data.author;
+}
+
+getQuote()
+
+run.addEventListener('click', getQuote)
